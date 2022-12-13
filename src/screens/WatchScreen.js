c94794a6497c056882f84a7c4ca11f9fdaad9a15
Ahmed Islam <modamoon48@gmail.com>
2022-12-13 15:45:57 +0300

    Cleaned watch screen

--- src/screens/WatchScreen.js
+++ src/screens/WatchScreen.js
@@ -6,7 +6,16 @@ import { StatusBar } from "expo-status-bar";
 const WatchScreen = ({ route }) => {
 	const { source } = route.params;
 
-	const html = `<html><head><meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0"></head><body style="margin: 0;"><video style="width: 100%; height: 100%; background: #000;" controls><source src=${source}>></video></body></html>`;
+	const html = `<html>
+						<head>
+							<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
+						</head>
+						<body style="margin: 0;">
+							<video style="width: 100%; height: 100%; background: #000;" controls>
+								<source src=${source}>
+							</video>
+						</body>
+					</html>`;
 
 	return (
 		<View style={{ flex: 1 }}>
