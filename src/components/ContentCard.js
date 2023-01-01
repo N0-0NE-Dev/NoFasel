93c33d4bba08745877207c0eebb5f63c7ee9fe74
Ahmed Islam <modamoon48@gmail.com>
2023-01-01 13:54:58 +1100

    A change of heart

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -12,14 +12,6 @@ const ContentCard = ({ navigation, imageSource, title, id, category }) => {
 				navigation.navigate("Select", { id: id, category: category })
 			}
 		>
-			<Image
-				style={styles.logoStyle}
-				source={
-					category.includes("arabic")
-						? require("../assets/Akwam.png")
-						: require("../assets/Fasel.png")
-				}
-			/>
 			<Image style={styles.imageStyle} source={{ uri: imageSource }} />
 			<Text numberOfLines={1} style={styles.textStyle}>
 				{title}
@@ -41,13 +33,6 @@ const styles = StyleSheet.create({
 		textAlign: "center",
 		fontSize: 16,
 		color: darkTheme ? "white" : "black",
-	},
-	logoStyle: {
-		width: 80,
-		height: 25,
-		position: "absolute",
-		zIndex: 1,
-		alignSelf: "flex-end",
 	},
 });
 
