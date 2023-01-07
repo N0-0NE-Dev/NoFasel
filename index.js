8819c2f4227649a148671093ec66e2ed86f866c2
Ahmed Islam <modamoon48@gmail.com>
2023-01-07 14:58:58 +1100

    Force LTR

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import App from "./App";
+import { I18nManager } from "react-native";
 
-import App from './App';
+I18nManager.forceRTL(false);
+I18nManager.allowRTL(false);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
