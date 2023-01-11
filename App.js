6961d9c8feb015c86fdfce73abefac8cd59833d5
Ahmed Islam <modamoon48@gmail.com>
2023-01-12 10:04:24 +1100

    Removed unused packages

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import "expo-dev-client";
-import "react-native-gesture-handler";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
